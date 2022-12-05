f6db4947f57fec303c5b7727934816de6396de74
morganwb <morgan@USHRV4-J4RG6L3.jade.jacobs.com>
2022-12-05 15:58:22 -0500

    Added changes to day_5

--- day_5/day_5.go
+++ day_5/day_5.go
@@ -4,8 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	// "strings"
-	// "strconv"
+	"strings"
+	"strconv"
 	// "sort"
 )
 
@@ -15,6 +15,7 @@ func main() {
 	// var part2Result int
 	var alphaBetVals = make(map[string]int)
 	var i,j int
+	// var err error
 	var cargoShip = make(map[string][]string)
 	var loadInitial bool = true
 	cargoShipRead := make([]string, 0)
@@ -41,16 +42,25 @@ func main() {
 								cargoShip[index] = append(cargoShip[index], string(cargoShipRead[j][i]))
 							}
 						}
-						fmt.Println("============================")
-						fmt.Println("here are the entries for:")
-						fmt.Println(index)
-						fmt.Println(cargoShip[index])
+						cargoShip[index] = cargoShip[index][1:len(cargoShip[index])]
+						fmt.Println(cargoShip[index][0])
 					}
 				}
 				loadInitial = false
 			}
+		} else if len(line) > 0{
+			// main sorting function goes here
+			cmdProc:= strings.Split(line, " ")
+			var moveCount, _ = strconv.Atoi(cmdProc[1])
+			moveFrom := cmdProc[3]
+			moveTo := cmdProc[5]
+			fmt.Println("============")
+			fmt.Printf("We want to move %d from %s to %s \n", moveCount, moveFrom, moveTo)
+			for i = 0; i<moveCount; i++ {
+				cargoShip[moveTo] = append(cargoShip[moveFrom][len(cargoShip[moveFrom])-1], cargoShip[moveTo])
+				cargoShip[moveFrom] = cargoShip[moveFrom][0:len(cargoShip[moveFrom])-2]
+			}
 		}
 	}
-	// fmt.Println(cargoShip)
-	// fmt.Println(part2Result)
+	fmt.Println(cargoShip)
 }
